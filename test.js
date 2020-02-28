0274d8264f397693f8fda26b1fffdfbe48b7592c
Liam <lgaskell@freewebstore.com>
2020-02-28 18:03:51 +0000

    Added more test cases and endpoints

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 const api = require('./api')({ base: 'http://34.249.159.220', port: '1624' });
-
-api.getServerStatus().then(console.log).catch(console.log);
+api.lookupPlayer("58E4E2DB").then(console.log).catch(console.log);
+api.cleanClientName("^4Liam^7Bobz43^");  //returns a string
